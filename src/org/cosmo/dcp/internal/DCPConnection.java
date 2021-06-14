f4d131c9ad5d948a517a5cb8fe792c5acc8e991f
Sonu Aryan <sonuaryan76448002@gmail.com>
2021-06-14 14:49:37 +0530

    init

---
+++ src/org/cosmo/dcp/internal/DCPConnection.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cosmo.dcp.internal;
+
+/**
+ *
+ * @author Sonu Aryan <cosmo-developer@github.com>
+ */
+public interface DCPConnection {
+    public static enum Type{
+        TX,RX
+    }
+}
